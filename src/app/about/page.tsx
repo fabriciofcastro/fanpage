44ac3fd54cf095267359b2f0570e99d5cc35f499
fabriciofeitosacastro@gmail.com <fabriciofeitosacastro@gmail.com>
2024-03-08 10:18:35 -0300

    Ajustado texto da page about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,14 +19,19 @@ const Page = () => {
         <h2 className={ style.heading }> SOBRE <span>MIN</span>  </h2>
         <h3>Front-end developer </h3>
         <p>
-          Atualmente estou em fase de transição de carreira. Busco uma oportunidade na área de desenvolvimento, especificamente como front-end para colocar em prática meus conhecimentos, aprender e contribuir junto a vossa empresa. Estou extremamente motivado e apaixonado pela área!
-          Fui proprietário na Loja de Informática Inforleste por 12 anos, desde jovem sempre me interessei e admirei o mundo da tecnologia e essa experiência me possibilitou entender o quanto a vida de um empresário é desafiadora, pois além das tarefas administrativas, também executava a parte operacional: manutenção em hardwares, instalação de softwares, redes, visitas presenciais em clientes, assistência remota, dentre outras funções relacionadas à área, sempre visando a satisfação do cliente. Muitos são amigos até o presente momento.
+          Sou um entusiasta apaixonado pelo mundo do desenvolvimento front-end. Desde que entrei em contato com a magia da criação de interfaces digitais, tenho buscado incansavelmente aprimorar minhas habilidades e conhecimentos nesta área emocionante além proporcionar conhecimentos paralelos.
           </p>
           <p>
-          Trabalhei nos últimos meses com o desenvolvimento de uma plataforma de (LMS) treinamentos corporativos online para executivos e colaboradores. O desenvolvimento aconteceu plataforma Bubble no code. Encarregado de criar páginas, layouts e responsivos, foi uma tarefa inicialmente tanto desafiadora por não saber nada sobre o conceito no code, devido meus conhecimentos em HTML, CSS e JS foi o diferencia no meu aprendizado por utilizar os mesmos conceitos de codificação. E junto com alguns colegas, estamos desenvolvendo um sistema que facilita a conversão de habitações brasileiras,  para condução em Portugal, também via Bubble. Se desejar consultar maiores detalhes deste trabalho, apresentarei com o maior prazer. 
+          Minha jornada começou com um fascínio pelo design e pela interatividade dos sites que eu visitava, o que me levou a explorar o universo do desenvolvimento web. Desde então, mergulhei de cabeça em aprender as linguagens fundamentais, como HTML, CSS e JavaScript, Reactjs, Nextjs absorvendo tudo o que podia por meio de cursos online, projetos pessoais e experiências práticas.
           </p>
           <p>
-          Comecei a estudar por conta própria desenvolvimento Web no início de 2023, iniciando 2024 na graduação de análise e desenvolvimento de sistema pela universidade Fiap, atualmente sou capaz de contruir interfaces utilizando HTML, CSS (sass, styled-componestent, css module), Javascript, React.js e Next.js.  Iniciei recentemente a estudar com axios, Redux.js 
+          Além das habilidades técnicas, também valorizo muito a colaboração e a comunicação eficaz. Acredito que o trabalho em equipe é essencial para o sucesso de qualquer projeto, e estou sempre disposto a contribuir com ideias e soluções criativas para alcançar os objetivos da equipe.
+        </p>
+        <p>
+          Estou animado com a oportunidade de aplicar minhas habilidades e conhecimentos em um ambiente profissional, onde posso contribuir para projetos desafiadores e fazer parte de uma equipe que compartilha minha paixão pela criação de experiências digitais excepcionais.
+        </p>
+        <p>
+          Se você está em busca de um desenvolvedor front-end dedicado, motivado e pronto para enfrentar novos desafios, adoraria ter a oportunidade de conversar mais sobre como posso contribuir para o sucesso da sua equipe.
         </p>
        
       </section>
