45459cde7d67dce28ea91237e79242f3ced879d9
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2024-08-03 01:16:46 -0300

    adjusted mobile elements

--- src/pages/home/homePage.tsx
+++ src/pages/home/homePage.tsx
@@ -14,19 +14,19 @@ export default function HomePage() {
         <title>Meu portfolio</title>
       </Head>
     
-      <div className="mt-24 grid grid-auto-fit-[30.5rem] min-md:grid-auto-fit min-xs:grid-auto-fit gap-12 py-[6rem] max-lg:py-10 max-w-screen-2xl m-auto  ">
+      <div className="mt-24 grid grid-auto-fit-[30.5rem] max-sm:grid-auto-fit gap-12 py-[6rem] max-lg:py-10 max-w-screen-2xl m-auto  ">
         <div className="flex justify-center px-10 flex-col space-y-8 max-lg:order-2">
           <div className="space-y-4">
             <div>
-              <h3 className="text-4xl max-lg:text-2xl">Olá!</h3>
-              <h1 className="text-6xl font-semibold max-lg:text-4xl">
+              <h3 className="text-4xl text-blue-900 max-lg:text-2xl max-md:text-lg">Olá!</h3>
+              <h1 className="text-6xl font-semibold max-lg:text-4xl max-md:text-2xl">
                 EU SOU <br />
                 DESENVOLVERDOR <br />
                 FRONT-END
               </h1>
             </div>
             <div>
-              <h3 className="text-2xl font-semibold">Seja Bem-vindo ao meu website</h3>
+              <h3 className="text-2xl font-semibold text-blueDark-A1">Seja Bem-vindo ao meu website</h3>
               <p>
                 Estou feliz por ter você aqui. Este é o centro onde mostro minha paixão por software, como serviço (SaaS), website e compartilho insights sobre as soluções que construo. Faça um tour para descobrir mais sobre o que ofereço e como podemos colaborar.
               </p>
