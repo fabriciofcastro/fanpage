7641bde7c1759c7a43d9e988ba2c420ebf960e5c
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2024-08-07 00:06:36 -0300

    Adjusted menu

--- src/app/about/components/navbar/navbarLinks.tsx
+++ src/app/about/components/navbar/navbarLinks.tsx
@@ -21,67 +21,67 @@ import { Card } from "@/components/ui/card"
 export default function NavbarLinks() {
 
   const [isCollapsed, setIsCollapsed] = useState(true)
-  
+
   function handleCollapsed() {
     setIsCollapsed(() => !isCollapsed)
   }
   return (
     <Card className="max-md:hidden">
-    <div className="px-3 pt-3">
-      <Button
-      className="rounded-full p-0 h-7 w-7 ml-3"
-        variant="secondary"
-        onClick={handleCollapsed}
-      >
-        {isCollapsed ? <ChevronRight size={18} /> : <ChevronLeft size={18} />}
-      </Button>
-      <Nav isCollapsed={isCollapsed}
-            links={[
-              {
-                title: "Sobre mim",
-                label: "",
-                icon: Inbox,
-                variant: "ghost",
-                href: "/about"
-              },
-              {
-                title: "Skills",
-                label: "",
-                icon: BicepsFlexed,
-                variant: "ghost",
-                href: "/about/skills"
-              },
-              {
-                title: "Formação",
-                label: "",
-                icon: GraduationCap ,
-                variant: "ghost",
-                href: "/about/academic"
-              },
-              {
-                title: "Projetos",
-                label: "",
-                icon: PanelsTopLeft,
-                variant: "ghost",
-                href: "/about/project"
-              },
-              {
-                title: "Trash",
-                label: "",
-                icon: Construction,
-                variant: "ghost",
-                href: "/about/services"
-              },
-              {
-                title: "Contato",
-                label: "",
-                icon: NotebookTabs,
-                variant: "ghost",
-                href: "/about/contact"
-              },
-            ]}
-          />
-    </div>
+      <div className="px-3 pt-3">
+        <Button
+          className="rounded-full p-0 h-7 w-7 ml-3"
+          variant="secondary"
+          onClick={ handleCollapsed }
+        >
+          { isCollapsed ? <ChevronRight size={ 18 } /> : <ChevronLeft size={ 18 } /> }
+        </Button>
+        <Nav isCollapsed={ isCollapsed }
+          links={ [
+            {
+              title: "Sobre mim",
+              label: "",
+              icon: Inbox,
+              variant: "ghost",
+              href: "/about"
+            },
+            {
+              title: "Skills",
+              label: "",
+              icon: BicepsFlexed,
+              variant: "ghost",
+              href: "/about/skills"
+            },
+            {
+              title: "Formação",
+              label: "",
+              icon: GraduationCap,
+              variant: "ghost",
+              href: "/about/academic"
+            },
+            {
+              title: "Projetos",
+              label: "",
+              icon: PanelsTopLeft,
+              variant: "ghost",
+              href: "/about/project"
+            },
+            {
+              title: "Trash",
+              label: "",
+              icon: Construction,
+              variant: "ghost",
+              href: "/about/services"
+            },
+            {
+              title: "Contato",
+              label: "",
+              icon: NotebookTabs,
+              variant: "ghost",
+              href: "/about/contact"
+            },
+          ] }
+        />
+      </div>
     </Card>
   )
 }
--- src/pages/header/header.tsx
+++ src/pages/header/header.tsx
@@ -9,14 +9,15 @@ import MenuNavigation from './links'
 function Header() {
 
   return (
-    <div className="flex justify-between h-24 px-5 bg-slate-400 dark:bg-slate-900 shadow-md w-full fixed top-0 left-0  z-10  ">
-      <div className="flex items-center justify-between flex-1 ">
+    <div className="flex items-center h-20 px-5 bg-slate-400 dark:bg-slate-900 shadow-md w-full fixed top-0 left-0  z-10  ">
+
+      <div className="flex items-center justify-between flex-1 max-w-screen-2xl m-auto ">
         <div className="flex items-center">
           <Logo />
         </div>
         <div className="flex items-center gap-2 ">
           <div className="lg:hidden ">
-            <Drawer  >
+            <Drawer >
               <DrawerTrigger asChild>
                 <Button >
                   <MenuResponsive />
