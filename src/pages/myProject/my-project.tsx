ddf76975dd6c04c8deece0eea28f73592b27f8db
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2024-08-14 18:36:29 -0300

    Adjusted link my projects

--- src/pages/myProject/my-project.tsx
+++ src/pages/myProject/my-project.tsx
@@ -10,6 +10,7 @@ import web7 from '@/assets/image/web7.avif';
 
 import { Button } from '@/components/ui/button';
 import { View } from 'lucide-react';
+import Link from 'next/link';
 import CardsProject from './CardsProject';
 
 export default function MyProject() {
@@ -25,7 +26,7 @@ export default function MyProject() {
         <div className="px-10 py-6 max-lg:px-0">
 
           <div className="grid grid-auto-fit-xl max-lg:grid-auto-fit-xl gap-10 justify-items-center max-lg:px-0">
-            <CardsProject title="Website para testar SCORM" description="Ajudamos plataforma de gestão de treinamento LMS " image={ web7 } linkUrl="https://leitorscorm.com.br"/>
+            <CardsProject title="Website para testar SCORM" description="Ajudamos plataforma de gestão de treinamento LMS " image={ web7 } linkUrl="https://leitorscorm.com.br" />
             <CardsProject title="Website Cyber Punk" description="Site para prover jogo de FPS" image={ web1 } />
             <CardsProject title="Website Gamer Top 10" description="Site ranking gamers Top 10 " image={ web2 } />
             <CardsProject title="Website analytical management" description="Dashboard informativo" image={ web3 } />
@@ -36,7 +37,9 @@ export default function MyProject() {
           </div>
           <div className="flex justify-center">
             <Button className="flex gap-2 mt-14 px-6">
-              Conheçer mais projetos
+              <Link href="/about/projects">
+                Conheçer mais projetos
+              </Link>
               <View strokeWidth={ 1.25 } />
             </Button>
           </div>
--- src/pages/sevices/services.tsx
+++ src/pages/sevices/services.tsx
@@ -1,13 +1,11 @@
 import CardGrid from '@/components/card/card-grid'
-import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Separator } from '@/components/ui/separator'
-import { Cable, Computer, MonitorPlay, Option, Palette, Presentation, View } from 'lucide-react'
+import { Cable, Computer, MonitorPlay, Option, Palette, Presentation } from 'lucide-react'
 
 export default function Services() {
   return (
     <div className="px-10 py-12 ">
-      <div className="max-w-screen-2xl m-auto">
+      <div className="max-w-screen-2xl m-auto ">
         <div className="flex py-10 gap-10 font-semibold">
           <h1 className="text-bg-blueDark-A1 text-4xl max-lg:text-2xl dark:text-bg-blueDark-A5">MEUS SERVIÇOS</h1>
           <Separator className="flex-1 flex self-center text-bg-blueDark-A1" />
