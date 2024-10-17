ddba7758a04f316e1e9491340978a9c2cf5303bb
Fabricio Castro <fabriciofeitosacastro@gmail.com>
2024-10-17 10:00:24 -0300

    adjusted menu mobile

--- src/pages/header/navbar-mobili/navbarMobili.tsx
+++ src/pages/header/navbar-mobili/navbarMobili.tsx
@@ -3,72 +3,20 @@
 import { Nav } from "@/app/about/components/navbar/navbar-about"
 import {
   BicepsFlexed,
+  Construction,
   GraduationCap,
   Inbox,
   NotebookTabs,
-  PanelsTopLeft,
-  Trash2,
-  Construction
+  PanelsTopLeft
 } from "lucide-react"
+
 
 export default function NavbarMobile() {
 
 
   return (
     <div className="px-3 pt-3">
-      <Nav 
-        isCollapsed={ false }
-        links={ [
-          {
-            title: "Home",
-            label: "",
-            icon: Inbox,
-            variant: "ghost",
-            href: "/"
-          },
-          {
-            title: "Sobre mim",
-            label: "",
-            icon: Inbox,
-            variant: "ghost",
-            href: "/about"
-          },
-          {
-            title: "Skills",
-            label: "",
-            icon: BicepsFlexed,
-            variant: "ghost",
-            href: "/about/skills"
-          },
-          {
-            title: "Formação",
-            label: "",
-            icon: GraduationCap,
-            variant: "ghost",
-            href: "/about/academic"
-          },
-          {
-            title: "Projetos",
-            label: "",
-            icon: PanelsTopLeft,
-            variant: "ghost",
-            href: "/about/projects"
-          },
-          {
-            title: "Serviços",
-            label: "",
-            icon: Construction,
-            variant: "ghost",
-            href: "/about/services"
-          },
-          {
-            title: "Contato",
-            label: "",
-            icon: NotebookTabs,
-            variant: "ghost",
-            href: "/about/contact"
-          },
-        ] }
+      <Nav isCollapsed={ false }
       />
     </div>
   )
